db2b0b55d66f7563f79eae3ef667934c8bf50cd0
Alwaz1143 <alwaz1143@gmail.com>
2024-09-18 10:55:51 +0530

    new bulb game

---
+++ bulb_using_js/script.js
@@ -0,0 +1,10 @@
+function turnoff(){
+    document.getElementById('bijlikabill').src = "";
+    document.getElementById('bulb').src = "off.png";
+}
+
+function turnon(){
+    document.getElementById('bulb').src = "on.png";
+    document.getElementById('bijlikabill').src = "bijli ka bill.jpeg";
+
+}
