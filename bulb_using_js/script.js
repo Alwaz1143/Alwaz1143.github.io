be66a3bc94db8bc177fa47bdb670c804b0dee6f9
Alwaz1143 <alwaz1143@gmail.com>
2024-11-21 16:47:13 +0530

    added audio

--- bulb_using_js/script.js
+++ bulb_using_js/script.js
@@ -1,10 +1,28 @@
-function turnoff(){
-    document.getElementById('bijlikabill').src = "";
-    document.getElementById('bulb').src = "off.png";
-}
 
-function turnon(){
-    document.getElementById('bulb').src = "on.png";
-    document.getElementById('bijlikabill').src = "bijli ka bill.jpeg";
+document.addEventListener("DOMContentLoaded", function(){
+    let toggle = document.querySelector(".switch");
+    let warning = document.querySelector(".warning");
+    let audio = new Audio("bijlee_ka_bill.mp3");
+    function showWarning(){
+        warning.style.visibility = "visible";
+    }
+    function hideWarning(){
+        warning.style.visibility = "hidden";
+    }
+    toggle.addEventListener("mouseover", showWarning);
+    toggle.addEventListener("mouseout", hideWarning);
+    toggle.addEventListener("click",function(){
+        if (document.getElementById('bulb').src.includes("off.png")){
+            document.querySelector('#bulb').src = "on.png";
+            document.querySelector('#bijlikabill').src = "bijli ka bill.jpeg";
+            toggle.src = "switch_on.png";
+            audio.play();
+        }
+        else if (document.getElementById('bulb').src.includes("on.png")){
+            document.querySelector('#bijlikabill').src = "";
+            document.querySelector('#bulb').src = "off.png";
+            toggle.src = "switch_off.png";
+        }
+    })
 
-}
+})
