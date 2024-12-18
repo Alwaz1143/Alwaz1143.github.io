9828af984a38de4fbb69bfd7fa7adc43244e55ea
Alwaz1143 <alwaz1143@gmail.com>
2024-12-18 12:22:12 +0530

    adding install feature

--- task-manager/service-worker.js
+++ task-manager/service-worker.js
@@ -1,25 +1,28 @@
+const CACHE_NAME = "task-manager-v1";
+const CACHE_URLS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/icons/icon1.png",
+  "/icons/icon2.png",
+  "checked.png",
+  "unchecked.png"
+];
+
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/script.js',
-          '/icons/icon.png',
-          '/icons/icon2.png',
-          '/checked.png',
-          '/unchecked.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching files...');
+      return cache.addAll(CACHE_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
